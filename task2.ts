53680bd240466639e46897b8074ef3a891b21f57
NstSidorova <sidorova_nst@mail.ru>
2026-03-09 20:09:30 +0300

    feature: resolve task2

--- task2.ts
+++ task2.ts
@@ -1,5 +1,13 @@
 // 1. Создайте функцию, которая создает массив с 10 случайными числами и возвращает произведение 3 самых больших значений
 
-function randomNumbers(arr: []) {
-  const cubeFaces = [2, 4, 6, 8, 10, 12, 20, 100];
+function createArray(size: number = 10, maxValue: number = 100, multiply: number = 3) {
+  let arr = [];
+  while (arr.length < size){
+    arr.push(Math.floor(Math.random() * maxValue)+1);
+  }
+  arr.sort((a,b) => b - a);
+  let resultArray = arr.splice(0, multiply);
+  const result = resultArray.reduce((acc, value) => acc*value);
+  return result;
 }
+console.log(createArray());
