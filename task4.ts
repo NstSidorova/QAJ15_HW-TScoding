0b7dbeddd7a3dc3087715c9942839d66a91f4f88
NstSidorova <sidorova_nst@mail.ru>
2026-03-14 19:14:24 +0300

    feat: resolve task 4

--- task4.ts
+++ task4.ts
@@ -1,1 +1,17 @@
 // 3. Создать функцию которая будет принимать входящее значение и добавлять к нему слово "супер"
+
+function addWord(word = 'котик') {
+  const containsCyrillic = /^[а-яёА-ЯЁ]+$/;
+  const containsLatin = /^[a-zA-Z]+$/;
+  if (containsCyrillic.test(word)) {
+    return 'супер' + word;
+  }
+  if (containsLatin.test(word)) {
+    return 'super' + word;
+  }
+  if (!containsCyrillic.test(word) || !containsLatin.test(word)) {
+    throw new Error('Строка должна содержать только кириллицу или только латиницу.');
+  }
+}
+console.log(addWord("12"));
+console.log(addWord("собака"));
