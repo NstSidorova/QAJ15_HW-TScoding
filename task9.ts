500bac7b53338c23e59a8c5aa12fbea913a79785
NstSidorova <sidorova_nst@mail.ru>
2026-03-17 22:47:52 +0300

    feat: resolve task 9,10 HW16

---
+++ task9.ts
@@ -0,0 +1,26 @@
+// 0. У нас поменялись требования к паролю. Юзерам выслана ссылка на смену пароля. Теперь когда они зайдут к нам на сайт, мы должны проверить, что их новый пароль соответствует следующим требованиям:
+//      - минимум 8 символов                            | латинские буквы, максимум 15
+//      - минимум одна заглавная буква
+//      - минимум одна цифра
+//      - минимум один специальный символ из набора     | ^!@_$&*()-+
+// а. Создайте функцию, которая будет принимать на вход строку (пароль) и возвращать true, если пароль соответствует требованиям и false, если не соответствует.
+// б. Нашим аналитикам интересно, какие цифры чаще всего используются юзерами в паролях. модифицируйте функцию так, чтобы она вместо булевого значения возвращала объект по следующим примерам:
+//   input: 'Password123!' -> output: { isValid: true, digits: [1, 2, 3] }
+//   input: 'myC00!Pa55w0rd' -> output: { isValid: true, digits: [0, 0, 5, 5, 0] }
+
+function checkPassword (pass: string) {
+  const passwordLengthMin = 8;
+  const passwordLengthMax = 15;
+  if (pass.length < passwordLengthMin || pass.length > passwordLengthMax || !/[A-Z]/g.test(pass) || !/[0-9]/g.test(pass) || /[А-Яа-яЁё]/g.test(pass) || !/[!^@_$&*()-+]/g.test(pass)) {
+    return false;
+  }
+  return true
+}
+console.log(checkPassword("fFffffffdd1+"))
+
+function checkDigits (password: string) {
+  const isValid = checkPassword(password)
+  const digits = password.match(/[0-9]/gi)
+  return { isValid, digits}
+}
+console.log(checkDigits("12GF22ggf+"))
